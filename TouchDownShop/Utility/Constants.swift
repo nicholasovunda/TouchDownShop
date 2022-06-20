7021c9875e0cb6482a8fca525017dd1c1dff3c13
nicholas <nicholasovunda@gmail.com>
2022-06-20 20:57:28 +0100

    First commit

---
+++ TouchDownShop/Utility/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  TouchDownShop
+//
+//  Created by Nicholas on 20/06/2022.
+//
+
+import Foundation
+import SwiftUI
+
+
+// DATA
+// COLOR
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+// UX
+// API
+
