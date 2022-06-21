6b2d3f1310bf0e49d64503621085fb1ea92d2f61
nicholas <nicholasovunda@gmail.com>
2022-06-21 07:57:45 +0100

    Implemented a responsive section, using the UIApplication windows.

--- TouchDownShop/App/ContentView.swift
+++ TouchDownShop/App/ContentView.swift
@@ -11,9 +11,34 @@ struct ContentView: View {
      // MARK: - PROPERTIES
     
       // MARK: - BODY
+//    func getSafeTopArea() -> CGFloat{
+//        let keyWindow = UIApplication.shared.connectedScenes
+//            .filter({$0.activationState == .foregroundActive})
+//            .map({$0 as? UIWindowScene})
+//            .compactMap({0})
+//            .first?.windows
+//            .filter({$0.isKeyWindow}).first
+//
+//        return keyWindow?.safeAreaInsets.top ?? 0
+//
+//    }
     var body: some View {
-        FooterView()
-            .padding(.horizontal)
+   
+        ZStack {
+            VStack (spacing: 0){
+                NavigationBarView()
+                    .padding(.horizontal, 15)
+                    .padding(.bottom)
+                    .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+                    .background(Color.white)
+                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
+                Spacer()
+                FooterView()
+                    .padding(.horizontal)
+            }//:VSTACK
+            .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+        } //:ZSTACK
+        .ignoresSafeArea(.all, edges: .top)
     }
 }
   // MARK: - PREVIEW
---
+++ TouchDownShop/Utility/LogoView.swift
@@ -0,0 +1,37 @@
+//
+//  LogoView.swift
+//  TouchDownShop
+//
+//  Created by Nicholas on 20/06/2022.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    var body: some View {
+        HStack(spacing: 4) {
+            Text("Touch".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+            
+            Image("logo-dark")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30, alignment: .center)
+            
+            Text("Down".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+        } //: HSTACK
+    }
+}
+
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
